cbe92bb9e8ff7166bd08fb0537e869bf9f84bb8b
domin <dominik.lepczynski@gmail.com>
2025-04-24 12:20:51 +0200

    platforms

--- custom_components/evopell/evopell.py
+++ custom_components/evopell/evopell.py
@@ -16,7 +16,7 @@ from .const import (
 _LOGGER = logging.getLogger(__name__)
 
 class Evopell(DataUpdateCoordinator):
-     def __init__(
+    def __init__(
         self, 
         hass: HomeAssistant,
         name,
@@ -34,4 +34,18 @@ class Evopell(DataUpdateCoordinator):
         self._host = host
         self._port = port
         self._scan_interval = scan_interval
-        
+        self.evopell_data = {}
+
+    def _update(self) -> dict:
+        """Update."""
+        try:
+            return self.evopell_data
+        except Exception as error:
+            raise UpdateFailed(error) from error
+    
+    async def _async_update_data(self) -> dict:
+        """Time to update."""
+        try:
+            return await self.hass.async_add_executor_job(self._update)
+        except Exception as exc:
+            raise UpdateFailed(f"Error updating evpell data: {exc}") from exc
---
+++ custom_components/evopell/sensor.py
@@ -0,0 +1,26 @@
+
+import logging
+from . import Evopell
+from homeassistant.components.select import SelectEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.const import (
+    CONF_NAME
+)
+
+from .const import (
+    DOMAIN, 
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    name = config_entry.data[CONF_NAME]
+    evopell = hass.data[DOMAIN][name]["evopell"]
+    
